6e9a9f26d2c627e4321e3b3801543b4a5f09c181
Iana Barkhatova <yanabarhat@gmail.com>
2025-01-22 10:47:52 +0200

    Implement removing and adding money

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import {Country} from "./Country";
 import {v1} from "uuid";
 
-export type BanknotsType = 'USD' | 'RUB' |'All'
+export type BanknotsType = 'USD' | 'RUB' | 'All'
 export type MoneyType = {
     banknote: BanknotsType
     nominal: number
@@ -23,7 +23,7 @@ let defaultMoney: MoneyType[] = [  // типизируем
 
 
 export const moneyFilter = (money: MoneyType[], filter: BanknotsType): MoneyType[] => {
-    if(filter === 'All') return money
+    if (filter === 'All') return money
     return money.filter(m => m.banknote === filter)
 }
 
@@ -35,15 +35,16 @@ function App() {
     const filteredMoney = moneyFilter(money, filterValue)
 
     const addMoney = (banknote: BanknotsType) => {
-        // Добавление денег сделаем в последнюю очередь, после настройки фильтров и отрисовки денег
+        const newMoney: MoneyType = {banknote, nominal: 100, id: v1()}
+        setMoney([...money, newMoney])
     }
 
     const removeMoney = (banknote: BanknotsType) => {
-        // Снятие денег сделаем в последнюю очередь, после настройки фильтров и отрисовки денег
-       // const index = money.findIndex
-       //  if (index !== -1) {
-       //      setMoney(money.filter((el, i) => ...));
-       //  }
+        const index = money.findIndex(el => el.banknote === banknote)
+
+        if (index !== -1) {
+            setMoney(money.filter((el, i) => i !== index));
+        }
     }
 
     return (
@@ -51,6 +52,8 @@ function App() {
             <Country
                 data={filteredMoney}   //отрисовать будем деньги после фильтрации
                 setFilterValue={setFilterValue}  //useState передаем? Так можно было?!
+                addMoney={addMoney}
+                removeMoney={removeMoney}
             />
         </div>
     );
